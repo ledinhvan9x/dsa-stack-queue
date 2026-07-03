72fcab853f1a6f5d0296d91985228784b999fd72
dinhvan <dinhvanle9xpro@gmail.com>
2026-07-03 15:37:05 +0700

    update stack

--- stack.js
+++ stack.js
@@ -1,3 +1,4 @@
+// way 1
 const stack = []
 
 // Push (add to the top)
@@ -28,3 +29,38 @@ console.log(stack.length)
 // Is Empty
 console.log(stack.length === 0)
 // false
+
+// way 2
+class Stack {
+    constructor() {
+        this.items = []
+    }
+
+    push(value) {
+        this.items.push(value)
+    }
+
+    pop() {
+        if (this.isEmpty()) return undefined
+        return this.items.pop()
+    }
+
+    peek() {
+        if (this.isEmpty()) return undefined
+        return this.items[this.items.length - 1]
+    }
+
+    isEmpty() {
+        return this.items.length === 0
+    }
+
+    size() {
+        return this.items.length
+    }
+
+    clear() {
+        this.items = []
+    }
+}
+
+// Best to worst: way 1 to way 2
